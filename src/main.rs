a11a4fd220d83418d25c9fe70c29de15f9d5ea98
Palladium02 <seanmeyer544@gmail.com>
2026-03-20 11:22:26 +0100

    wired everything together

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
 #![feature(new_range_api)]
+#![feature(anonymous_lifetime_in_impl_trait)]
 #![allow(unused)]
+mod analysis;
+mod args;
+mod export;
 mod grammar;
+mod traits;
 
 use std::fs;
 
-use crate::grammar::{analysis::Analyzer, lexer::Lexer, parser::Parser};
+use clap::Parser as ArgParser;
+
+use crate::{
+    analysis::analyzer::Analyzer,
+    args::{Args, Command},
+    export::graphviz::Graphviz,
+    grammar::{lexer::Lexer, parser::Parser},
+};
 
 fn main() {
-    let input = fs::read_to_string("./examples/example1.ptr").unwrap();
-    let ast = Parser::new(Lexer::new(&input)).parse();
+    let args = Args::parse();
 
-    match ast {
-        Ok(ast) => {
-            println!("{:#?}", &ast);
-            let report = Analyzer::new(ast).analyze();
-            println!("{:#?}", report)
-        }
-        Err(_) => println!("Whoopsie daisy"),
+    let input = fs::read_to_string(args.command.input()).expect("Failed to read input");
+    let ast = Parser::new(Lexer::new(&input))
+        .parse()
+        .expect("Failed to parse");
+    let (model, report) = Analyzer::analyze(&ast);
+
+    println!("{}", report.to_string(&input));
+
+    if report.has_error() {
+        return;
+    }
+
+    match args.command {
+        Command::Export { output, engine, .. } => Graphviz::export(&model, output, &engine),
+        Command::Interactive { input } => todo!(),
     }
 }
